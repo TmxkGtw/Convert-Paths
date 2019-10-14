893c8f133d55910f88827b14c60ee7380974fa2e
Tomoaki Goto <elessar869@gmail.com>
2019-10-14 14:39:03 +0900

    [CommentFix] Accomplish commentary

--- modules/ConvertTo-RelativePaths/ConvertTo-RelativePaths.psm1
+++ modules/ConvertTo-RelativePaths/ConvertTo-RelativePaths.psm1
@@ -4,49 +4,80 @@ function ConvertTo-RelativePaths{
         This function converts absolute paths into their relative path counterparts.
     .DESCRIPTION
         This function has following merits:
-            1. 
+            1. It can convert paths into the absolutes no matter whether they really exist or not
+            2. It can accept direct pipeline input of `Get-ChildItem` commandlet
+            3. It can change the basepath of relative path calculation by the parameter `BasePath`
+
+        In contrast, native `Resolve-Path -Relative` has following demerits:            
+            1. It can not convert unexistent paths
+            2. It can not accept direct pipeline input of `Get-ChildItem` commandlet
+            3. It can only calculate relativepath based on the current location
     .PARAMETER AbsolutePaths
-        
+        This parameter accepts absolute paths.
+    .PARAMETER BasePath
+        This parameter accepts basal path for calculating relative paths.
     .PARAMETER RealPathsOnly
-
+        This switch parameter decides whether all paths are conveted or only really existent paths are converted.
     .INPUTS
-
+        In this function, the parameter `AbsolutePaths` accepts pipeline inputs of absolute paths.
+        The parameter can also accepts pipeline inputs of FullName attribute values by its alias `FullName`
     .OUTPUTS
-
+        This function returns `[string[]]` typed array of relative paths.
     .EXAMPLE
-    
+        PS C:\sample> ConvertTo-RelativePaths -AbsolutePaths C:\aaa.txtS, C:\bbb.txt
+    .EXAMPLE
+        PS C:\sample> ConvertTo-RelativePaths -AbsolutePaths C:\aaa.txt, C:\bbb.txt -BasePath ..\
+    .EXAMPLE
+        PS C:\sample> ConvertTo-RelativePaths -AbsolutePaths C:\aaa.txt, C:\bbb.txt -RealPathsOnly
+    .EXAMPLE
+        PS C:\sample> @("C:\aaa.txt", "C:\bbb.txt")|ConvertTo-RelativePaths
+    .EXAMPLE
+        PS C:\sample> $(Get-ChildItem)|ConvertTo-RelativePaths    
     #>
 
     [OutputType([String[]])]
     [CmdletBinding()]
     param(
         [ValidateNotNullOrEmpty()]
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias("FullName")]
         [String[]] $AbsolutePaths,
         [ValidateNotNullOrEmpty()]
         [String]   $BasePath = $(Get-Location),
         [Switch]   $RealPathsOnly
     )
 
-    $originalLocation = $(Get-Location)
-    [System.IO.Directory]::SetCurrentDirectory($originalLocation)
+    begin{
+        $originalLocation = $(Get-Location)
+        [System.IO.Directory]::SetCurrentDirectory($originalLocation)
 
-    if($RealPathsOnly){
-        Set-Location -LiteralPath $BasePath
-        $relativePaths = $(Resolve-Path -Relative -LiteralPath $AbsolutePaths);
-        Set-Location -LiteralPath $originalLocation
-        return $relativePaths
+        if($RealPathsOnly){
+            Set-Location -LiteralPath $BasePath
+        }
     }
 
-    return $(
-        $AbsolutePaths.ForEach{
-            $rawRelativePath = [System.IO.Path]::GetRelativePath($BasePath, $_)
-            if($rawRelativePath -match "[^.]*\.[^.]+"){
-                $rawRelativePath
-            }else{
-                $rawRelativePath + "\"
-            }
+    process{
+        if($RealPathsOnly){            
+            return $(Resolve-Path -Relative -LiteralPath $AbsolutePaths);
         }
-    );
+    
+        return $(
+            $AbsolutePaths.ForEach{
+                $rawRelativePath = [System.IO.Path]::GetRelativePath($BasePath, $_)
+                if($rawRelativePath -match "[^.\\/]*\.[^.\\/]+"){
+                    $rawRelativePath
+                }else{
+                    $rawRelativePath + "\"
+                }
+            }
+        );
+    }
+
+    end{
+        if($RealPathsOnly){
+            Set-Location -LiteralPath $originalLocation
+        }
+    }    
 }
 
 Export-ModuleMember -Function *;
